e8844b466315a60273a41f9230bfbdd3d123395d
Carlos Henrique <ch94ca@gmail.com>
2020-10-12 10:31:41 -0300

    minor changes

--- Fork/Create n processes and rnd numbers/src/Main.cpp
+++ Fork/Create n processes and rnd numbers/src/Main.cpp
@@ -83,7 +83,7 @@ int main(int argc, char **argv)
 
         for (unsigned int i = 0; i < rndNumbersQty; i++)
         {
-                rndNumbers[i] = rand() % 10;
+                rndNumbers[i] = rand() % 100;
 
         } // end for
 
